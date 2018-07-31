67fbcaa4884adfc5e892800ce71da4678b232e8f
Ioana <ioana.m.gadinceanu@gmail.com>
2018-07-31 16:55:55 +0300

    Initial commit

---
+++ creaturesOfTheSea/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  creaturesOfTheSea
+//
+//  Created by Ioana Gadinceanu on 7/13/18.
+//  Copyright © 2018 Apress. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
